ae5d351415b724991a6a6bdfc689026d7afc6917
bengHak <qkwl4678@naver.com>
2021-02-06 13:14:24 +0900

    refactor: edit url

--- backend/app.js
+++ backend/app.js
@@ -1,21 +1,21 @@
-const express = require('express')
-const pool = require('./config/dbPool')
-const authRouter = require('./router/auth')
-const ideaRouter = require('./router/idea')
-const mypageRouter = require('./router/mypage')
-const app = express()
-app.use(express.json())
-app.use(pool)
+const express = require("express");
+const pool = require("./config/dbPool");
+const authRouter = require("./router/auth");
+const ideaRouter = require("./router/idea");
+const mypageRouter = require("./router/mypage");
+const app = express();
+app.use(express.json());
+app.use(pool);
 
-app.use('/auth', authRouter)
-app.use('/idea', ideaRouter) // TODO: file 받기
-app.use('/mypage', mypageRouter)
+app.use("/api/auth", authRouter);
+app.use("/api/idea", ideaRouter); // TODO: file 받기
+app.use("/api/mypage", mypageRouter);
 // TODO : Mainpage에 띄울 것
 
-app.use('/', function(req, res) {
-    res.status(200).json({'msg' : 'success'})
-})
+app.use("/", function (req, res) {
+  res.status(200).json({ msg: "success" });
+});
 
 app.listen(3001, () => {
-    console.log("App is running on port 3001")
-})
+  console.log("App is running on port 3001");
+});
